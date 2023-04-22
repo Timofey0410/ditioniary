d1bd00f231759bed54b73cf8b607f29c9992e59e
Timofey0410 <131515731+Timofey0410@users.noreply.github.com>
2023-04-22 12:21:52 +0500

    Тема урока Dictionary

---
+++ main (1).py
@@ -0,0 +1,8 @@
+car = {"Toyota" : "rav4" , "Nisan" : "silvia" , "zhigul" : "devatka"}
+print(car)
+car["lada"] = "calina"
+print(car)
+car["zhigul"] = "vosymerka"
+print(car)
+del car["lada"]
+print(car)
